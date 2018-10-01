6293a84628c29a69cd27561724552bcf1caeadb3
Adrian Gierakowski <agierakowski@gmail.com>
2018-10-01 11:22:40 +0100

    remove asyncError helper

--- index.js
+++ index.js
@@ -10,27 +10,23 @@ var platforms = {
 
 function makeMethod(methodName) {
 	exports[methodName] = function (platform, payment, cb) {
-		function asyncError(message) {
-			var error = new Error(message);
-			// this will cause the stack trace to start at the place where asyncError
-			// is called
-			error.captureStackTrace(error, asyncError);
-			process.nextTick(cb, error);
-		}
-
 		if (!payment) {
-			return asyncError('No payment given');
+			return process.nextTick(cb, new Error('No payment given'));
 		}
 
 		var engine = platforms[platform];
 
 		if (!engine) {
-			return asyncError(`Platform ${platform} not recognized`);
+			return process.nextTick(
+				cb,
+				new Error(`Platform ${platform} not recognized`)
+			);
 		}
 
 		if (!engine[methodName]) {
-			return asyncError(
-				`Platform ${platform} does not have a ${methodName} method`
+			return process.nextTick(
+				cb,
+				new Error(`Platform ${platform} does not have a ${methodName} method`)
 			);
 		}
 
